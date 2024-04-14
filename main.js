d55ed0794ca3e0f804684dbff5695972ebab7dfb
ukahchu <ukahchu@gmail.com>
2024-04-14 21:59:54 +0400

    complete pupular flight destinations section, complete app sectoin, complete testimonials and footer

--- main.js
+++ main.js
@@ -20,8 +20,69 @@ navLinks.addEventListener("click", (e) => {
 const bookingType = document.getElementById("booking-type");
 
 bookingType.addEventListener("click", (e) => {
-    Array.from(bookingType.getElementsByTagName("div")).forEach((item) => {
-        item.classList.remove("active")
-    });
-    e.target.classList.add("active");
+  Array.from(bookingType.getElementsByTagName("div")).forEach((item) => {
+    item.classList.remove("active");
+  });
+  e.target.classList.add("active");
+});
+
+//Start functionality for testimonial slider
+const swiper = new Swiper(".swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  autoplay: {
+    delay: 3000,
+  },
+});
+//End functionality for testimonial slider
+
+//Start of footer-year
+
+document.addEventListener("DOMContentLoaded", function () {
+  var currentYear = new Date().getFullYear(); // Get the current year
+  var footerYearDiv = document.getElementById("footer-year"); // Get the div by its ID
+  if (footerYearDiv) {
+    footerYearDiv.textContent = `© ${currentYear} Visa Travel Agency. All rights reserved.`; // Set the text content of the div
+    //adding styles to the footer
+    footerYearDiv.style.padding = "1rem";
+    footerYearDiv.style.fontSize = "0.8rem";
+    footerYearDiv.style.color = "var(--white)";
+    footerYearDiv.style.textAlign = "center";
+  }
+});
+//End of footer-year
+
+//start ofscroll reveal functionality
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+//header container
+ScrollReveal().reveal(".header__container h1", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header__container p", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header__container .booking", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+//service container
+ScrollReveal().reveal(".flights__card", {
+  duration: 1000,
+  interval: 500,
+});
+
+//offer container
+ScrollReveal().reveal(".offer__card", {
+  ...scrollRevealOption,
+  interval: 500,
 });
